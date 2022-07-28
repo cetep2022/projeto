59749b50dd9a182fb2ec1864428dfe45ce998314
Kelliton <macielcetep@gmail.com>
2022-07-28 13:56:58 -0300

    Adicionando tela inicial

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
+import Home from '../pages/Home/Home';
 
 function App() {
     return (
         <div className="App">
-            <div className="App__background">
-
-            </div>
+            <Home></Home>
         </div>
     )
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,33 @@
+import "./style.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1 className="Home__title">Apresentação do trabalho</h1>
+
+      <p className="Home__desc">
+        Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite
+        divinis, qui tem lupuliz, matis, aguis e fermentis.Mauris nec dolor in
+        eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Não
+        sou faixa preta cumpadi, sou preto inteiris, inteiris.Posuere libero
+        varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.
+        Casamentiss faiz malandris se pirulitá.Em pé sem cair, deitado sem
+        dormir, sentado sem cochilar e fazendo pose.Quem num gosta di mim que
+        vai caçá sua turmis!Interagi no mé, cursus quis, vehicula ac nisi. Quem
+        num gosta di mé, boa gentis num é.Copo furadis é disculpa de bebadis,
+        arcu quam euismod magna.Em pé sem cair, deitado sem dormir, sentado sem
+        cochilar e fazendo pose.Sapien in monti palavris qui num significa nadis
+        i pareci latim. Copo furadis é disculpa de bebadis, arcu quam euismod
+        magna.Quem num gosta di mé, boa gentis num é.Si num tem leite então bota
+        uma pinga aí cumpadi!Praesent vel viverra nisi. Mauris aliquet nunc non
+        turpis scelerisque, eget. Praesent malesuada urna nisi, quis volutpat
+        erat hendrerit non. Nam vulputate dapibus.Tá deprimidis, eu conheço uma
+        cachacis que pode alegrar sua vidis.Detraxit consequat et quo num tendi
+        nada.Quem manda na minha terra sou euzis! Detraxit consequat et quo num
+        tendi nada.Atirei o pau no gatis, per gatis num morreus.Praesent
+        malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate
+        dapibus.In elementis mé pra quem é amistosis quis leo.
+      </p>
+    </div>
+  );
+}
