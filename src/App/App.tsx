2b1c461078eb50b080e6fe9d98c286aa1f0dba18
Kelliton <macielcetep@gmail.com>
2022-07-28 15:03:11 -0300

    Adicionando nav

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Cards from '../components/Cards'
 import Home from '../pages/Home/Home';
+import Header from '../components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Home></Home>
       <div className="layout-app">
         <div className='container'>
