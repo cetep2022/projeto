42b81281f68c8bbcacceb790e0990ef791825538
maciel <macielcetep@gmail.com>
2022-07-28 14:39:58 -0300

    add cards

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Cards from '../components/Cards'
 
 function App() {
-    return (
-        <div className="App">
-
-        </div>
-    )
+  return (
+    <div className="layout-app">
+      <div className='container'>
+        <Cards heading="teste" message="sdsh jsdfhsj jd fj kf skfj  fjfks f sfjs fjsfkjs  fsf" button="git-hub" />
+        <Cards heading="teste" message="sdsh jsdfhsj jd fj kf skfj  fjfks f sfjs fjsfkjs  fsf" button="git-hub" />
+      </div>
+    </div>
+  )
 }
 
 export default App
