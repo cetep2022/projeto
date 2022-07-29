b0b5725c1f7e9e891c42e81c0205ad1b498ca2a5
maciel <macielcetep@gmail.com>
2022-07-29 14:03:07 -0300

    add cards

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,7 @@ import Cards from '../pages/Cards/Cards';
 import Home from '../pages/Home/Home';
 import Header from '../components/Header';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header></Header>
