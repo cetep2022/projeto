3fc4385bf51f79fdf131e1e759d9cf43228fef6c
maciel <macielcetep@gmail.com>
2022-07-29 14:04:22 -0300

    add

---
+++ src/pages/Cards/Cards.tsx
@@ -0,0 +1,21 @@
+import './Cards.css'
+export default function Cards() {
+    return (
+       <div className="d-flex">
+          <div className="d-width-card">
+            <h2>Meu perfil 1</h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam omnis quas aut harum maiores ipsum corporis sint laboriosam vero similique laudantium, nesciunt repudiandae aspernatur, expedita fugiat ex, animi possimus esse!
+            </p>
+            <a href="">git-hub</a>
+          </div>
+          <div className="d-width-card">
+            <h2>Meu perfil 2</h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam omnis quas aut harum maiores ipsum corporis sint laboriosam vero similique laudantium, nesciunt repudiandae aspernatur, expedita fugiat ex, animi possimus esse!
+            </p>
+            <a href="">git-hub</a>
+          </div>
+       </div>
+    )
+}
