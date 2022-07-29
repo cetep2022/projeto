d69212555116e5720d18bf427847dc1a571eb3e9
Kelliton <macielcetep@gmail.com>
2022-07-29 14:46:21 -0300

    Adicionando perfil

--- src/pages/Cards/Cards.tsx
+++ src/pages/Cards/Cards.tsx
@@ -1,28 +1,35 @@
-
-import './Cards.css'
+import "./Cards.css";
 export default function Cards() {
-    return (
-      <div className='d-container'>
-         <div className="d-flex">
-          <div className="d-width-card">
-            <h2>My Perfil Profissional</h2>
-            <p>
-                Hi word, I am develop web. Usually I'm study a litle English and languagens programation. start at 8:00Am, some 
-            </p>
-            <div className='d-button-flex'>
-             <a href="">git-hub</a>
-           </div>
+  return (
+    <div className="d-container">
+      <div className="d-flex">
+        <div className="d-width-card">
+          <h2>My Perfil Profissional</h2>
+          <p>
+            Hi word, I am develop web. Usually I'm study a litle English and
+            languagens programation. start at 8:00Am, some
+          </p>
+          <div className="d-button-flex">
+            <a href="">git-hub</a>
           </div>
-          <div className="d-width-card">
-            <h2>Meu perfil 2</h2>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam omnis quas aut harum maiores ipsum corporis sint laboriosam vero similique laudantium, nesciunt repudiandae aspernatur, expedita fugiat ex, animi possimus esse!
-            </p>
-           <div className='d-button-flex'>
-             <a href="">git-hub</a>
-           </div>
+        </div>
+        <div className="d-width-card">
+          <h2>Kelliton Santana</h2>
+          <p>
+            Habilidades: <br></br>- CSS <br></br>- HTML <br></br>- JavaScript{" "}
+            <br></br>- ReactJS <br></br>- Sass / Scss / Less <br></br>- NodeJS{" "}
+            <br></br>- Git <br></br>- Figma
+            <br></br>
+            <br></br>
+            Front-end com habilidades em CSS, HTML, JavaScript, React.JS, Sass,
+            etc. Atualmente no 3 ano do ensino médio técnico, cursando
+            informática.
+          </p>
+          <div className="d-button-flex">
+            <a href="">git-hub</a>
           </div>
-       </div>
+        </div>
       </div>
-    )
+    </div>
+  );
 }
